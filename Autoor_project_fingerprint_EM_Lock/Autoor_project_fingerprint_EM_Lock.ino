ae2c491a8611f8cf33d9fb83c061bd8de40d2fe7
myeongseok-dsm1101 <tametabby1@naver.com>
2023-03-25 02:48:31 +0900

    completed

--- Autoor_project_fingerprint_EM_Lock/Autoor_project_fingerprint_EM_Lock.ino
+++ Autoor_project_fingerprint_EM_Lock/Autoor_project_fingerprint_EM_Lock.ino
@@ -114,26 +114,3 @@ uint8_t getFingerprintID() {
   
   return finger.fingerID;
 }
-
-// returns -1 if failed, otherwise returns ID #
-//int getFingerprintIDez() {
-//  uint8_t p = finger.getImage();
-//  if (p != FINGERPRINT_OK)  return -1;
-//
-//  p = finger.image2Tz();
-//  if (p != FINGERPRINT_OK)  return -1;
-//
-//  p = finger.fingerFastSearch();
-//  if (p != FINGERPRINT_OK)  return -1;
-//  
-//  // found a match!
-//  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
-//  Serial.print(" with confidence of "); Serial.println(finger.confidence);
-//  
-//  // when match fingerprint, release LOCK.
-//
-//  digitalWrite(LOCK, HIGH);
-//  delay(5000);
-//  digitalWrite(LOCK, LOW);
-//  return finger.fingerID; 
-//}
